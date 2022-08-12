f5f61087308aa92cf08b096bf21a8987faf1183a
Michael George <mdgeorge@cs.cornell.edu>
2022-08-12 16:51:47 -0400

    fixed message

--- certora/scripts/verifyPool.sh
+++ certora/scripts/verifyPool.sh
@@ -6,6 +6,6 @@ certoraRun \
     --verify Pool:certora/specs/pool_link.spec \
     --solc solc8.0 \
     --link Pool:asset=Asset \
-    --msg "Pool with linking" \
+    --msg "Pool complete spec" \
     --send_only
 
